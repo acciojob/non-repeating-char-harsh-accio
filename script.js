f0631cf6db99283560eefeab7126786f46587b81
harsh-accio <rajputharsh9897@gmail.com>
2025-09-18 11:43:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	 let freq = {};
+
+  // Count frequency of each character
+  for (let char of str) {
+    freq[char] = (freq[char] || 0) + 1;
+  }
+
+  // Find the first char with frequency = 1
+  for (let char of str) {
+    if (freq[char] === 1) {
+      return char;
+    }
+  }
+
+  return null; // if none found
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
